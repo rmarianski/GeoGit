a6b9abf713b142a80e6fc4e5a35d4fb2e8e56548
Gabriel Roldan <gabriel.roldan@gmail.com>
2014-04-01 01:52:16 -0300

    set version to 0.7.1

--- doc/workshops/source/conf.py
+++ doc/workshops/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2013, GeoGit Contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '0.7-SNAPSHOT'
+version = '0.7.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.7'
 
