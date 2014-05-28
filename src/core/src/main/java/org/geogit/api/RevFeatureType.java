8dab88903beccc5f34df828e16e0c599d173ce64
Victor Olaya <volayaf@gmail.com>
2014-05-29 00:17:02 +0200

    Avoid feature type from using DefaultGeographicCRS.WGS84,
    It causes inconsistent behaviour when storing in the database and then taking back the CRS from it

--- src/core/src/main/java/org/geogit/api/RevFeatureType.java
+++ src/core/src/main/java/org/geogit/api/RevFeatureType.java
@@ -4,15 +4,21 @@
  */
 package org.geogit.api;
 
-import java.util.ArrayList;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.geotools.referencing.crs.DefaultGeographicCRS.WGS84;
 
 import org.geogit.api.plumbing.HashObject;
+import org.geotools.data.DataUtilities;
+import org.geotools.referencing.CRS;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.Name;
 import org.opengis.feature.type.PropertyDescriptor;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 
 /**
  * A binary representation of the state of a Feature Type.
@@ -47,11 +53,27 @@ public class RevFeatureType extends AbstractRevObject {
      */
     public RevFeatureType(ObjectId id, FeatureType featureType) {
         super(id);
+        checkNotNull(featureType);
+        CoordinateReferenceSystem defaultCrs = featureType.getCoordinateReferenceSystem();
+        if (WGS84.equals(defaultCrs)) {
+            // GeoTools treats DefaultGeographic.WGS84 as a special case when calling the
+            // CRS.toSRS() method, and that causes the parsed RevFeatureType to hash differently.
+            // To compensate that, we replace any instance of it with a CRS built using the
+            // EPSG:4326 code, which works consistently when storing it and later recovering it from
+            // the database.
+            checkArgument(featureType instanceof SimpleFeatureType);
+            try {
+                final boolean longitudeFirst = true;
+                CoordinateReferenceSystem epsg4326 = CRS.decode("EPSG:4326", longitudeFirst);
+                String[] includeAllAttributes = null;
+                featureType = DataUtilities.createSubType((SimpleFeatureType) featureType,
+                        includeAllAttributes, epsg4326);
+            } catch (Exception e) {
+                throw Throwables.propagate(e);
+            }
+        }
         this.featureType = featureType;
-        ArrayList<PropertyDescriptor> descriptors = Lists.newArrayList(this.featureType
-                .getDescriptors());
-        sortedDescriptors = ImmutableList.copyOf(descriptors);
-
+        this.sortedDescriptors = ImmutableList.copyOf(featureType.getDescriptors());
     }
 
     @Override
